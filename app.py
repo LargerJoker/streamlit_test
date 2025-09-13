87405fa996f27df3d2022d91e0980f706fdbeb81
LargerJoker <diego98vg@gmail.com>
2025-09-12 21:03:23 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 import scipy.stats
 import streamlit as st
 import time
+import pandas as pd
+
+# estas son variables de estado que se conservan cuando Streamlit vuelve a ejecutar este script
+if 'experiment_no' not in st.session_state:
+  st.session_state['experiment_no'] = 0
+
+if 'df_experiment_results' not in st.session_state:
+  st.session_state['df_experiment_results'] = pd.DataFrame(columns = ['no', 'iteraciones', 'media')
 
 st.header('Lanzar una moneda')
 
 chart = st.line_chart([0.5])
 
 #Funcion que emula el lanzamiento de una moneda
-def toos_coin(n):
+def toss_coin(n):
   trial_outcomes = scipy.stats.bernoulli.rvs(p = 0.5, size = n)
 
   mean = None
